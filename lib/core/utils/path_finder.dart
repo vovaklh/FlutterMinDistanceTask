53720aebd0e8997739a92cffcfa98652517f0840
Vladimir <vovaklh20012@gmail.com>
2022-10-12 18:26:06 +0300

    dev: add unit tests

--- lib/core/utils/path_finder.dart
+++ lib/core/utils/path_finder.dart
@@ -25,6 +25,10 @@ class _BFGPathFinder implements _PathFinder {
 
   @override
   Queue<Point> findShortestPath() {
+    // if (data.start == data.end) {
+    //   return Queue.from([data.start]);
+    // }
+
     grid = List.generate(
       data.field.length,
       (index) => data.field[index].split(''),
